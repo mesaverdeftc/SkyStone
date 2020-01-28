53e7777fe7ecbb86883b79b41db8ca20e2c45d0d
alex-battikha <alex.battikha@gmail.com>
2020-01-27 19:27:33 -0800

    New TestMotor class to test the motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotors.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="TestMotors", group="Linear Opmode")
+//@Disabled
+public class TestMotors extends LinearOpMode{
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    private DriveTrain driveTrain = new DriveTrain();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        driveTrain.init(hardwareMap);
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !driveTrain.imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("imu calib status", driveTrain.imu.getCalibrationStatus().toString());
+
+        //waiting for start
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        sleep(1000);
+        driveTrain.gyroDrive(this, runtime, 0.7, 100, 0, 30);
+        sleep(1000);
+        driveTrain.gyroDrive(this, runtime, -0.7, -100, 0, 30);
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+    }
+}

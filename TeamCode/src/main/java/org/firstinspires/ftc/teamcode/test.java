b0f3c125a6bf3c04a8e120737d930d560d69df3c
rotate0 <hudson.n.kim@gmail.com>
2020-09-14 18:51:37 -0700

    WIP: OpenCV for ring detection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@Autonomous(name="test", group="Linear Opmode")
+//@Disabled
+public class test extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    private DriveTrain driveTrain = new DriveTrain();
+    private Foundation foundation = new Foundation();
+    private Attachment block = new Attachment();
+    private Attachment grabber = new Attachment();
+    private DistanceSensor distanceSensor = null;
+    private ColorDistance colorDistance = new ColorDistance();
+    private int offset = 0;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        driveTrain.init(hardwareMap);
+
+        colorDistance.init(hardwareMap, "block_color_2");
+        distanceSensor = hardwareMap.get(com.qualcomm.robotcore.hardware.DistanceSensor.class, "distance_1");
+        block.init(hardwareMap, "block_servo0", 0, 1.0);
+        grabber.init(hardwareMap, "grabber_servo4", 1.0, 0);
+        foundation.init(hardwareMap, "foundation_servo1", "foundation_servo2", 1.0, 0);
+        foundation.up();
+        block.up();
+        grabber.up();
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !driveTrain.imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("imu calib status", driveTrain.imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        //waiting for start
+        telemetry.addData("Mode", "waiting for start");
+
+        waitForStart();
+
+
+        driveTrain.rotate(this, 1, .6);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+    }
+}
